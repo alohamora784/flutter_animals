00168f6f83dd41072e7b423410ae6e286c4993ef
George Gagishvili <g.gagishvili@noxtton.com>
2023-03-17 15:12:53 +0400

    fix

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:animal_buttons/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget( AnimalButtons());
+    await tester.pumpWidget( const AnimalButtons());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
